38c787d99c31d20fbfe01c399a34673ad791a3c1
HellenNzisa <hellen.mutemi@strathmore.edu>
2025-06-20 15:41:31 +0300

    Completed assignment question X

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,20 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    n = len(unsorted_list)
+
+    swapped = False
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                # Swap if the element found is greater than the next element
+                unsorted_list[j], unsorted_list[j + 1] = (
+                    unsorted_list[j + 1],
+                    unsorted_list[j],
+                )
+                swapped = True
+
+        if not swapped:
+            # If no two elements were swapped by inner loop, then break
+            break
+
+    return unsorted_list
